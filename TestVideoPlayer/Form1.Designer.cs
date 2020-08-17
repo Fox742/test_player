f13111a3741603992c333dd61e69e1929b7ce0d7
Your Name <igr.khitrov@gmail.com>
2020-08-17 22:30:17 +0300

    changed form desighn

--- TestVideoPlayer/Form1.Designer.cs
+++ TestVideoPlayer/Form1.Designer.cs
@@ -38,9 +38,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(47, 80);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(231, 38);
+            this.button1.Size = new System.Drawing.Size(266, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load shedule";
             this.button1.UseVisualStyleBackColor = true;
