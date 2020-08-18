58b7b11df4be4160ff4fe55a81151352eeeceefe
Your Name <igr.khitrov@gmail.com>
2020-08-18 13:31:18 +0300

    Some changes in VideoController

--- TestVideoPlayer/VideoController.cs
+++ TestVideoPlayer/VideoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace VideoPlayerEngine
 {
@@ -11,17 +12,39 @@ namespace VideoPlayerEngine
     /// </summary>
     class VideoController
     {
+        /// <summary>
+        /// Флаг, который сигнализирует о смене расписания. При взведённом флаге контроллер должен работать немного по-другому нежели в "Штатном режиме"
+        /// </summary>
+        private bool changingShedule = false;
+
+        private FutureEventsList CurrentFEList = null;
+        private FutureEventsList CandidateFEList = null;
+        private Mutex candidateLock = new Mutex();
+
+
         public VideoController()
-        {
-
-        }
-
-        public void LoadShedule(Shedule shedule)
         {
             InterfaceWrapper.VideoEndEvent += OnVideoEnd;
         }
 
+        public void LoadShedule(Shedule shedule)
+        {
+            FutureEventsList newList = new FutureEventsList(shedule);
+
+
+            candidateLock.WaitOne();
+            changingShedule = true;
+            CandidateFEList = newList;
+            candidateLock.ReleaseMutex();
+
+        }
+
         private void OnVideoEnd()
+        {
+
+        }
+
+        private void OnFELEvent()
         {
 
         }
